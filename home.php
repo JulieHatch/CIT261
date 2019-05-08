46ea2218a3b4deb6e0a1f0a6c33393b2f3b8b571
Julie <dav15036@byui.edu>
2019-05-08 15:59:53 -0600

    My first commit

---
+++ home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Julie Hatch Code Topics</title>
+</head>
+<body>
+	<h1> Welcome to Code Topics for CIT 261</h1><br>
+	<a href="home.html">JavaScript(Week 3)</a><br>
+	<a href="puppy.html">JavaScript Objects(Week 3)</a><br>
+	<a href="home.html">AJAX(Week 4)</a><br>
+	<a href="home.html">Local Storage(Week 4)</a><br>
+	<a href="home.html">CSS3 Transitions(Week 5)</a><br>
+	<a href="home.html">CSS3 Animations(Week 6)</a><br>
+	<a href="home.html">CSS3 Transitions & Animations(Week 6)</a><br>
+	<a href="home.html">DOM(Week 7)</a><br>
+	<a href="home.html">HTML5 Tags(Week 7)</a><br>
+	<a href="home.html">JSON(Week 8)</a><br>
+	<a href="home.html">CSS Class Properties Using JavaScript(Week 8)</a><br>
+	<a href="home.html">JavaScript Events(Week 9)</a><br>
+	<a href="home.html">CSS3 Transforms(Week 9)</a><br>
+</body>
+</html>
