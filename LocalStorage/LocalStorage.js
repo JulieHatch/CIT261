5608d6e585fb6aa8e224151dc499be5ac3f63f4b
Julie <dav15036@byui.edu>
2019-05-18 15:05:05 -0600

    examples of session and local storage

--- LocalStorage/LocalStorage.js
+++ LocalStorage/LocalStorage.js
@@ -3,5 +3,23 @@
  * this page.
 **********************************************************/
 function start(){
+	document.getElementById("color").innerHTML = 
+	sessionStorage.color;
+	document.getElementById("food").innerHTML = 
+	localStorage.food;
+}
+
+/**********************************************************
+ * The function that sets off the rest of the functions for
+ * this page.
+**********************************************************/
+function setFavorites(){
+	// set variables
+	var color = document.getElementById("color").value;
+	var food  = document.getElementById("food").value;
 	
+	// now store variables
+	sessionStorage.color = color;
+	localStorage.food    = food;
+	start();
 }
