924bfc98326a098ef92ad708cbc5ca8cc32d91d8
Julie <dav15036@byui.edu>
2019-05-18 15:45:56 -0600

    examples of session storing array

--- LocalStorage/LocalStorage.js
+++ LocalStorage/LocalStorage.js
@@ -7,11 +7,13 @@ function start(){
 	localStorage.color;
 	document.getElementById("dFood").innerHTML = 
 	sessionStorage.food;
+	document.getElementById("arrayEx").innerHTML = 
+	sessionStorage.animals;
 }
 
 /**********************************************************
- * The function that sets off the rest of the functions for
- * this page.
+ * The function sets favorite food and color of a user and
+ * stores the data in local and session storage.
 **********************************************************/
 function setFavorites(){
 	// set variables
@@ -19,7 +21,34 @@ function setFavorites(){
 	var food  = document.getElementById("food").value;
 	
 	// now store variables
-	localStorage.color = color;
-	sessionStorage.food    = food;
+	localStorage.color  = color;
+	sessionStorage.food = food;
+	start();
+}
+
+/**********************************************************
+ * This function sets an array based on user input and 
+ * stores the array in session storage.
+**********************************************************/
+function setArray(){
+	// set variables
+	var dog = document.getElementById("dog").value;
+	var cat  = document.getElementById("cat").value;
+	var bird = document.getElementById("bird").value;
+	var bear  = document.getElementById("bear").value;
+	
+	var animals = [dog, cat, bird, bear];
+	// store in session storage
+	sessionStorage.animals = animals;
+	start();
+}
+
+/**********************************************************
+ * This function sets an object based on user input and 
+ * stores the object in local storage.
+**********************************************************/
+function setArray(){
+	// set variables
+
 	start();
 }
