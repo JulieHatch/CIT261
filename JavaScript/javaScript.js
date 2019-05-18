212cfb5daa0dd99a07bb770d0be674099a668012
Julie <dav15036@byui.edu>
2019-05-18 12:35:20 -0600

    adding conditional statement examples

--- JavaScript/javaScript.js
+++ JavaScript/javaScript.js
@@ -2,6 +2,15 @@
  * Simple examples of how to use for loops, while loops,
  * and do while loops. Nothing really exciting.
 **********************************************************/
+function start(){
+	looper();
+	condStatment();
+}
+
+/**********************************************************
+ * Simple examples of how to use for loops, while loops,
+ * and do while loops. Nothing really exciting.
+**********************************************************/
 function looper(){
 	//declare variables
 	var i;
@@ -9,24 +18,24 @@ function looper(){
 	var stuffWhile   = "";
 	var stuffDoWhile = "";
 	
-	//This is a simple for loop
-	for (i=0; i <= 4;i++){
-		stuffFor += "For loop for four times.<br>";
+	// This is a simple for loop
+	for (i=0; i < 4;i++){
+		stuffFor += "Loop for four times.<br>";
 	}
 	document.getElementById("lFor").innerHTML = stuffFor;
 	i=0; // reset i
 	
-	//This is a simple while loop
+	// This is a simple while loop
 	while (i<4){
-		stuffWhile+= "Looped through while loop" + i + " times.<br>";
+		stuffWhile+= "Looped through while loop " + i + " times.<br>";
 		i++;
 	}
 	document.getElementById("lWhile").innerHTML = stuffWhile;
 	i=0; // reset i
 	
-	//This is a simple do while loop
+	// This is a simple do while loop
 	do{
-		stuffDoWhile+= "Looped through do while loop" + i + " times.<br>";
+		stuffDoWhile+= "Looped through do while loop " + i + " times.<br>";
 		i++;
 	}
 	while(i<=3);
@@ -34,8 +43,8 @@ function looper(){
 }
 
 /**********************************************************
- * Simple examples of how to use for loops, while loops,
- * and do while loops. Nothing really exciting.
+ * Simple examples of how to use conditional statements
+ * such as if, else, else if, and switch statements.
 **********************************************************/
 function condStatment(){
 	//declare variables
@@ -43,6 +52,52 @@ function condStatment(){
 	var j = 2;
 	var k = 3;
 	
-	//This is a simple if statement
+	// This is a simple if statement
+	if (i == 1){
+		document.getElementById("sIF").innerHTML = 
+		"Conditional statement, \"if (i == 1)\", is true.";
+	}
 	
+	// This is a simple example of an else statement
+	if (i == 2){
+		document.getElementById("sElse").innerHTML = 
+		"Conditional statement, \"if (i == 2)\", is true.";
+	}
+	else{
+		document.getElementById("sElse").innerHTML = 
+		"Conditional statement, \"if (i == 2)\", is false.";
+	}
+	
+	// This is a simple example of else if statements
+	if (i == 2){
+		document.getElementById("sElseIf").innerHTML = 
+		"Conditional statement, \"if (i == 2)\", is true.";
+	}
+    else if(i == 1){
+		document.getElementById("sElseIf").innerHTML = 
+		"Conditional statement, \"if (i == 2)\", is false."+
+		"However, conditional statement, \"if (i == 1)\", is true.";
+	}
+	else{
+		document.getElementById("sElseIf").innerHTML = 
+		"Conditional statement, \"if (i == 2)\", is false."+
+		"and conditional statement, \"if (i == 1)\", is false.";
+	}
+	
+	// This is a simple example of a switch statement
+	switch (i){
+		case 0:
+			document.getElementById("sSwitch").innerHTML = "oops";
+			break;
+		case 1:
+			document.getElementById("sSwitch").innerHTML = 
+			"Case 1 was true!";
+			break;
+		case 2:
+			document.getElementById("sSwitch").innerHTML = "oops";
+			break;
+		case 3:
+			document.getElementById("sSwitch").innerHTML = "oops";
+			break;
+	}
 }
