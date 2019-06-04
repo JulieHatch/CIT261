d4b0e478da14bab3244b3ea7226b5667e3f0b8e0
Julie <dav15036@byui.edu>
2019-06-04 16:05:16 -0600

    added insertBefore example

--- DOM/DOM.js
+++ DOM/DOM.js
@@ -1,14 +1,6 @@
 /**********************************************************
- * The function that sets off the rest of the functions for
- * this page.
-*********************************************************
-function start(){
-
-}*/
-
-/**********************************************************
  * This function shows a simple example of a createElement
- * and appendChild
+ * and appendChild.
 **********************************************************/
 function sCreateEx(){
 	// get the div element I wish to add the new element that I created.
@@ -18,5 +10,25 @@ function sCreateEx(){
 	created.innerHTML = " This text was added after you clicked the button!";
 	// add the new element to div tag with id ="sCreateEx"
 	sCreateEx.appendChild(created);
+}
+
+/**********************************************************
+ * This function shows a simple example of a insertBefore
+ * in the situation of an ordered list.
+**********************************************************/
+function sInsertEx(){
+	// get the list number placement the user wishes item to placed in the list
+	var sInsertNum  = document.getElementById("sInsertNum").value;
+	// get the item the user wishes to be added to the list.
+	var sInsertItem = document.getElementById("sInsertItem").value;
+	
+	// create the element
+	var iCreated  = document.createElement("li");
+	var iText     = document.createTextNode(sInsertItem);
+	iCreated.appendChild(iText);
+	
+	// get the div element I wish to add the new element that I created.
+	var sInsertEx = document.getElementById("sInsertEx");
+    sInsertEx.insertBefore(iCreated, sInsertEx.childNodes[sInsertNum]);
 }
 
