f85410454b05e03afe7515bae64d4b2bc666386b
Julie <dav15036@byui.edu>
2019-06-18 08:30:15 -0600

    adding animations simple examples

---
+++ CSS3_JS - Copy/CSS3_JS.js
@@ -0,0 +1,7 @@
+/**********************************************************
+ * The function that sets off the rest of the functions for
+ * this page.
+*********************************************************
+function start(){
+
+}*/
