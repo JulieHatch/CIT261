3be2c08eb46ecad9dc9e053020e12cdb56ff3ee5
Julie <dav15036@byui.edu>
2019-06-22 13:06:52 -0600

    JS Events added examples to page

---
+++ JavaScriptEvents/JavaScriptEvents.js
@@ -0,0 +1,31 @@
+/**********************************************************
+ * Will display user's name once they click away from the 
+ * input
+**********************************************************/
+function changing(){
+	var change = document.getElementById("change").value;
+	document.getElementById("changing").innerHTML = change;
+}
+
+/**********************************************************
+ * Will display some text when the user clicks text.
+**********************************************************/
+function click(){
+	document.getElementById("click").innerHTML = "You clicked the text!";
+}
+
+/**********************************************************
+ * Will display user's name once they click away from the 
+ * input
+**********************************************************/
+function touch(){
+	document.getElementById("touch").innerHTML = "You touched the text!";
+}
+
+/**********************************************************
+ * Will display user's name once they click away from the 
+ * input
+**********************************************************/
+function load(){
+	document.getElementById("load").innerHTML = "This was loaded";
+}
