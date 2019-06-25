7730adfb56f4c7ffddb167dde4448b5c7fac5b54
Julie <dav15036@byui.edu>
2019-06-25 12:22:30 -0600

    adding another transition example

--- Trans_Anim_JS/Trans_Anim_JS.js
+++ Trans_Anim_JS/Trans_Anim_JS.js
@@ -1,13 +1,14 @@
 /**********************************************************
- * Starts the Transition and switches the transition if the
- * user requests it
+ * Starts the Transition and switches the color transition 
+ * if the user requests it 
 **********************************************************/
 function colorTrans(){
 	// Grab and set the values I will be using.
 	var circle      = document.getElementById('transition');
 	var buttonTrans = document.getElementById('buttonTrans');
 	
-	// check if it is blue or orange then changes color and button text
+	// check if it is blue or orange then changes color and 
+	// button text
 	if (buttonTrans.innerHTML == 'Orange'){
 		circle.style.backgroundColor = '#ef8a1f';
 		buttonTrans.innerHTML        = "Blue";
@@ -19,13 +20,37 @@ function colorTrans(){
 }
 
 /**********************************************************
- * Starts the Animation
+ * Starts the Transition, the transition changes shape from
+ * circle to square or square to circle upon user's request.
 **********************************************************/
-function pauseResume(){
+function shapeTrans(){
+	// Grab and set the values I will be using.
+	var circle        = document.getElementById('transition');
+	var butShapeTrans = document.getElementById('butShapeTrans');
+	
+	// check if it is a circle or a square then changes the shape
+	// and the button text
+	if (butShapeTrans.innerHTML == 'Circle'){
+		circle.style.borderRadius = '50px';
+		butShapeTrans.innerHTML   = "Square";
+	}
+	else {
+		circle.style.borderRadius = '0px';
+		butShapeTrans.innerHTML   = "Circle";
+	}
+}
+
+/**********************************************************
+ * Allows the user to pause and resume an Animation that 
+ * changes a shapes color
+**********************************************************/
+function colorPauseResume(){
 	// Grab and set the values I will be using.
 	var circle     = document.getElementById('animation');
 	var buttonAnim = document.getElementById('buttonAnim');
 	
+	// checks if it is running or paused then changes play 
+	// state and button text
 	if (buttonAnim.innerHTML == 'Pause'){
 		circle.style.animationPlayState = 'paused';
 		buttonAnim.innerHTML            = "Resume";
@@ -35,3 +60,24 @@ function pauseResume(){
 		buttonAnim.innerHTML            = "Pause";
 	}
 }
+
+/**********************************************************
+ * Allows the user to pause and resume an Animation that 
+ * changes a shapes color
+*********************************************************
+function colorPauseResume(){
+	// Grab and set the values I will be using.
+	var circle     = document.getElementById('animation');
+	var buttonAnim = document.getElementById('buttonAnim');
+	
+	// checks if it is running or paused then changes play 
+	// state and button text
+	if (buttonAnim.innerHTML == 'Pause'){
+		circle.style.animationPlayState = 'paused';
+		buttonAnim.innerHTML            = "Resume";
+	}
+	else {
+		circle.style.animationPlayState = 'running';
+		buttonAnim.innerHTML            = "Pause";
+	}
+}*/
