406a8aa5784aa8dcc5ecd5286fa170a190b3d75e
Julie <dav15036@byui.edu>
2019-06-25 11:32:00 -0600

    fixed transition example

--- Trans_Anim_JS/Trans_Anim_JS.js
+++ Trans_Anim_JS/Trans_Anim_JS.js
@@ -3,22 +3,25 @@
  * user requests it
 **********************************************************/
 function colorTrans(){
-	var box = document.getElementById('transition');
-	var buttonTrans = document.getElementById('buttonTrans').innerHTML;
-	// check if it is blue or orange
-	if (box.style.background == '#3c576d'){
-		box.style.background = '#ef8a1f';
-		buttonTrans          = "Blue";
+	// Grab and set the values I will be using.
+	var box         = document.getElementById('transition');
+	var buttonTrans = document.getElementById('buttonTrans');
+	
+	// check if it is blue or orange then changes color and button text
+	if (buttonTrans.innerHTML == 'Orange'){
+		box.style.backgroundColor = '#ef8a1f';
+		buttonTrans.innerHTML = "Blue";
 	}
 	else {
-		box.style.background = '#3c576d';
-		buttonTrans          = "Orange";
+		box.style.backgroundColor = '#3c576d';
+		buttonTrans.innerHTML = "Orange";
 	}
 }
 
 /**********************************************************
  * Starts the Animation
-**********************************************************/
+*********************************************************
 function colorAnim(){
 	
 }
+*/
