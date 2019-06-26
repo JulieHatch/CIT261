ddeb8d7baee32ef68a331e0ec4894b13c1c721d1
Julie <dav15036@byui.edu>
2019-06-26 14:57:12 -0600

    added funtions to play, pause, mute audio and video

--- HTML5_Tags/HTML5_Tags.js
+++ HTML5_Tags/HTML5_Tags.js
@@ -1,7 +1,81 @@
+/**********************Video Functions**********************/
 /**********************************************************
- * The function that sets off the rest of the functions for
- * this page.
-*********************************************************
-function start(){
+ * Plays Video
+**********************************************************/
+function playVideo(){
+	var video = document.getElementById("video").play();
+}
 
-}*/
+/**********************************************************
+ * Pauses Video
+**********************************************************/
+function pauseVideo(){
+	var video = document.getElementById("video").pause();
+}
+
+/**********************************************************
+ * Checks if video is muted then mutes it or unmutes it
+**********************************************************/
+function muteVideo(){
+	// Declare and set variables used in this function
+	var video 		 = document.getElementById("video");
+	var videoMuteBut = document.getElementById("videoMuteBut");
+	
+	// Checks if muted or not
+	if (video.volume == 0) {
+		// if muted unmute volume
+		video.volume 		   = 1;
+		// OR
+		// video.muted         = false;
+		videoMuteBut.innerHTML = "Mute";
+	}
+	else {
+		// if not muted mute volume
+		video.volume 		   = 0;
+		// OR 
+		// video.muted         = true;
+		videoMuteBut.innerHTML = "Unmute";
+	}
+}
+
+/**********************Audio Functions**********************/
+/**********************************************************
+ * Plays audio
+**********************************************************/
+function playAudio(){
+	document.getElementById("audio").play();
+}
+
+/**********************************************************
+ * Pauses audio
+**********************************************************/
+function pauseAudio(){
+	document.getElementById("audio").pause();
+}
+
+/**********************************************************
+ * Checks if audio is muted then mutes it or unmutes it
+**********************************************************/
+function muteAudio(){
+	// Declare and set variables used in this function
+	var audio		 = document.getElementById("audio");
+	var audioMuteBut = document.getElementById("audioMuteBut");
+	
+	// Checks if muted or not
+	if (audio.volume == 0) {
+		// if muted unmute volume
+		audio.volume		   = 1;
+		// OR
+		// audio.muted         = false;
+		audioMuteBut.innerHTML = "Mute";
+	}
+	else {
+		// if not muted mute volume
+		audio.volume		   = 0;
+		// OR 
+		// audio.muted         = true;
+		audioMuteBut.innerHTML = "Unmute";
+	}
+}
+
+/**********************Canvas Functions**********************/
