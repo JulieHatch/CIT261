8be443b0721b94479b89fced58fe0137f823b053
Julie <dav15036@byui.edu>
2019-06-27 15:10:44 -0600

    fiddling with JSON PARSE

--- AJAX/AJAX.js
+++ AJAX/AJAX.js
@@ -4,22 +4,20 @@
 **********************************************************/
 function getWeather(){
 	// Declare and set variables to be used in this function.
-	var zip		= document.getElementById("zipCode").value;
+	var lon		= document.getElementById("lon").value;
+	var lan		= document.getElementById("lan").value;
 	var weather = document.getElementById("weather");
-	
-	request(zip);
-
-}
-
-function request(zip) {
-	var api = "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139";
+	var api     = "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139";
 	var xhttp = new XMLHttpRequest();
+	var info = "";
+	var i;
 	xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("weather").innerHTML =
-      this.responseText;
-    }
-  };
+		if (this.readyState == 4 && this.status == 200) {
+		  info = JSON.parse(this.responseText);
+		  document.getElementById("weather").innerHTML = info.main.temp;
+		}
+	};
 	xhttp.open("GET", api, true);
 	xhttp.send();
+
 }
