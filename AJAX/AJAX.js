bc842fc94e7934799d97ba30307d4b7fb0f4904d
Julie <dav15036@byui.edu>
2019-06-27 14:42:28 -0600

    adding weather request

--- AJAX/AJAX.js
+++ AJAX/AJAX.js
@@ -1,8 +1,3 @@
-const aws = require('aws-sdk');
-
-let s3 = new aws.S3({
-  accessKeyId: process.env.key
-});
 /**********************************************************
  * Gets the current weather of a specific zip code 
  * specified by the user.
@@ -12,8 +7,19 @@ function getWeather(){
 	var zip		= document.getElementById("zipCode").value;
 	var weather = document.getElementById("weather");
 	
-	var mykey = config.MY_KEY;
-	// Call function to send request
-	weather.innerHTML = s3.accessKeyId;
-	// Call function to translate request
+	request(zip);
+
+}
+
+function request(zip) {
+	var api = "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139";
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("weather").innerHTML =
+      this.responseText;
+    }
+  };
+	xhttp.open("GET", api, true);
+	xhttp.send();
 }
