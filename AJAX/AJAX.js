53562857f279e9246cd4fb15c7dbddde8cee6a19
Julie <dav15036@byui.edu>
2019-06-27 12:03:35 -0600

    testing config

--- AJAX/AJAX.js
+++ AJAX/AJAX.js
@@ -1,3 +1,9 @@
+const aws = require('aws-sdk');
+
+let s3 = new aws.S3({
+  accessKeyId: process.env.S3_KEY,
+  secretAccessKey: process.env.S3_SECRET
+});
 /**********************************************************
  * Gets the current weather of a specific zip code 
  * specified by the user.
