4079eab8fe65651c12f3882647609a06a3c55cf9
Julie <dav15036@byui.edu>
2019-06-27 15:16:50 -0600

    added lat and lon inputs

--- AJAX/AJAX.js
+++ AJAX/AJAX.js
@@ -4,10 +4,11 @@
 **********************************************************/
 function getWeather(){
 	// Declare and set variables to be used in this function.
+	var lat		= document.getElementById("lat").value;
 	var lon		= document.getElementById("lon").value;
-	var lan		= document.getElementById("lan").value;
 	var weather = document.getElementById("weather");
-	var api     = "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139";
+	var api     = "https://fcc-weather-api.glitch.me/api/current?lat="
+				+ lat + "&lon=" + lon;
 	var xhttp = new XMLHttpRequest();
 	var info = "";
 	var i;
