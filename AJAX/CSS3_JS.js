f685fea180710cc54b941e9e159561685836d86a
Julie <dav15036@byui.edu>
2019-06-27 11:45:25 -0600

    adding .gitignore and config.js

---
+++ AJAX/CSS3_JS.js
@@ -0,0 +1,13 @@
+/**********************************************************
+ * Gets the current weather of a specific zip code 
+ * specified by the user.
+**********************************************************/
+function getWeather(){
+	// Declare and set variables to be used in this function.
+	var zip		= document.getElementById("zipCode").value;
+	var weather = document.getElementById("weather");
+	
+	// Call function to send request
+	
+	// Call function to translate request
+}
--- HTML5_Tags - Copy/CSS3_JS.js
+++
@@ -1,7 +0,0 @@
-/**********************************************************
- * The function that sets off the rest of the functions for
- * this page.
-*********************************************************
-function start(){
-
-}*/
