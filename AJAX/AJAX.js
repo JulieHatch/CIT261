3ca589541d6ed8fddada10ee0c69109c21c4f617
Julie <dav15036@byui.edu>
2019-06-27 17:16:36 -0600

    finishing touches

--- AJAX/AJAX.js
+++ AJAX/AJAX.js
@@ -6,19 +6,22 @@ function getWeather(){
 	// Declare and set variables to be used in this function.
 	var lat		= document.getElementById("lat").value;
 	var lon		= document.getElementById("lon").value;
-	var weather = document.getElementById("weather");
+	// set the url based on what the user input
 	var api     = "https://fcc-weather-api.glitch.me/api/current?lat="
 				+ lat + "&lon=" + lon;
 	var xhttp = new XMLHttpRequest();
 	var info = "";
-	var i;
+	
+	// request and process the data
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-		  info = JSON.parse(this.responseText);
-		  document.getElementById("temp").innerHTML    = info.main.temp;
-		  document.getElementById("high").innerHTML    = info.main.temp_max;
-		  document.getElementById("low").innerHTML     = info.main.temp_min;
-		  document.getElementById("weather").innerHTML = info.weather[0].main;
+			// parse the information received
+			info = JSON.parse(this.responseText);
+			// place these elements on the page
+			document.getElementById("temp").innerHTML    = info.main.temp;
+			document.getElementById("high").innerHTML    = info.main.temp_max;
+			document.getElementById("low").innerHTML     = info.main.temp_min;
+			document.getElementById("weather").innerHTML = info.weather[0].main;
 		}
 	};
 	xhttp.open("GET", api, true);
